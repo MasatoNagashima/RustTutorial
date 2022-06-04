73145f3f50aa0f46af7aed6843f3af91a0225773
MasatoN <yuuoumaishin@akane.waseda.jp>
2022-06-04 17:13:57 +0900

    change main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
-use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
 
 fn main() {
-    let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-
-    for stream in listner.incoming() {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
+
 
 
---
+++ target/debug/deps/hello-e5cc7a191a1d1056.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/hukki/OneDrive/デスクトップ/Codes/2022_Tasks/Tutorials/Rust/hello/target/debug/deps/hello-e5cc7a191a1d1056: src/main.rs
+
+/mnt/c/Users/hukki/OneDrive/デスクトップ/Codes/2022_Tasks/Tutorials/Rust/hello/target/debug/deps/hello-e5cc7a191a1d1056.d: src/main.rs
+
+src/main.rs:
